30cbd185dc37076f13e77ae4c57472c35b9f3003
NikolaL87 <dzoni.lovic@gmmail.com>
2018-11-01 21:24:26 +0100

    User Register and Login. Add errors for input fields.

--- client/src/components/register/RegisterForm.js
+++ client/src/components/register/RegisterForm.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import { Field, reduxForm } from 'redux-form';
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 import { Button, Divider } from 'semantic-ui-react';
 import { InputField } from '../shared/form/InputField';
 import { validate } from '../shared/form/Validators';
+import { connect } from 'react-redux';
 
-let RegisterForm = props => {
-	const { handleSubmit, submitting, errors } = props;
-	return (
-		<form onSubmit={handleSubmit}>
-			<Field name="firstName" errors={errors.firstName} component={InputField} placeholder="First Name" type="text" />
-			<Field name="email" errors={errors.email} component={InputField} placeholder="First Name" type="email" />
-			<Field name="password" errors={errors.password} component={InputField} placeholder="First Name" type="password" />
-			<Field name="passwordConfirmation" errors={errors.passwordConfirmation} component={InputField} placeholder="First Name" type="password" />
-			<Divider hidden />
-			<Button type="submit" secondary disabled={submitting}>
-				Register
-			</Button>
-		</form>
-	);
-};
+class RegisterForm extends Component {
+	constructor() {
+		super();
+		this.state = {
+			errors: {}
+		};
+
+		this.onChange = this.onChange.bind(this);
+	}
+	onChange(e) {
+		this.setState({ [e.target.name]: e.target.value });
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.errors) {
+			this.setState({ errors: nextProps.errors });
+		}
+	}
+	render() {
+		const { errors } = this.state;
+		return (
+			<form noValidate onSubmit={this.handleSubmit}>
+				<InputField
+					name="name"
+					type="text"
+					label="Your Name"
+					value={this.state.value}
+					onChange={this.onChange}
+					error={errors.name}
+				/>
+				<InputField
+					name="email"
+					type="email"
+					label="Your Email"
+					value={this.state.value}
+					onChange={this.onChange}
+					error={errors.email}
+				/>
+				<InputField
+					name="password"
+					type="password"
+					label="Your Password"
+					value={this.state.value}
+					onChange={this.onChange}
+					error={errors.password}
+				/>
+				<InputField
+					name="password2"
+					type="password"
+					label="Confirm Password"
+					value={this.state.value}
+					onChange={this.onChange}
+					error={errors.password2}
+				/>
+				<Divider hidden />
+				<Button type="submit" secondary>
+					Register
+				</Button>
+			</form>
+		);
+	}
+}
+
+const mapStateToProps = state => ({
+	errors: state.errors
+});
 
 RegisterForm = reduxForm({
 	// a unique name for the form
@@ -26,4 +78,4 @@ RegisterForm = reduxForm({
 	validate
 })(RegisterForm);
 
-export default RegisterForm;
+export default connect(mapStateToProps)(RegisterForm);
